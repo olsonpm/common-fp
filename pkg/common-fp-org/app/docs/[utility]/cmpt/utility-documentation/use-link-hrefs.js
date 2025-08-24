097dc5ace80e390936c5a99702f87b351aea6f21
Phil Olson <olsonpm@users.noreply.github.com>
2025-08-24 12:58:50 -0500

    wip2

--- pkg/common-fp-org/app/docs/[utility]/cmpt/utility-documentation/use-link-hrefs.js
+++ pkg/common-fp-org/app/docs/[utility]/cmpt/utility-documentation/use-link-hrefs.js
@@ -1,7 +1,5 @@
 import { useContext } from 'react'
 import { kebabCase } from 'change-case'
-import { repoBaseUrl } from '@/config'
-import { stripTrailingSlashes } from '@/utils'
 import simpleJoinPath from '@/utils/simple-join-path'
 import { PageCtx } from '../../page-context'
 
@@ -9,7 +7,7 @@ const useLinkHrefs = () => {
   const { utility } = useContext(PageCtx)
   if (!utility) return
 
-  const baseUrl = stripTrailingSlashes(repoBaseUrl)
+  const baseUrl = 'https://github.com/common-fp/common-fp/blob/main'
   const srcDir =
     baseUrl +
     simpleJoinPath([`/pkg/common-fp/src/lib`, utility.srcDir || '', '/'])
